661c72f7374ed620f088600853e7eacd1d24bfd0
mlaursen <mlaursen03@gmail.com>
2014-02-19 15:14:16 -0500

    removed old and worthless files for annotations. created new export jar

--- src/com/github/mlaursen/database/objects/DatabaseCreateable.java
+++
@@ -1,6 +0,0 @@
-package com.github.mlaursen.database.objects;
-
-public interface DatabaseCreateable {
-	boolean create();
-	String[] getCreateableParameters();
-}
--- src/com/github/mlaursen/database/objects/DatabaseListable.java
+++
@@ -1,8 +0,0 @@
-package com.github.mlaursen.database.objects;
-
-import java.util.List;
-
-public interface DatabaseListable {
-	String[] getListableParameters();
-	<T extends DatabaseListable> List<T> getAll(Class<T> type);
-}
