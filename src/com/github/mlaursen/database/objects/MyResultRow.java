2872108ba65584202d3999102c78ad8d559d8a78
mlaursen <mlaursen03@gmail.com>
2014-03-05 11:46:41 -0500

    added getInt and getDouble to automatically try parsing the string as an int or double

--- src/com/github/mlaursen/database/objects/MyResultRow.java
+++ src/com/github/mlaursen/database/objects/MyResultRow.java
@@ -7,7 +7,7 @@ import java.util.Map;
 /**
  * A representation of a database row
  * 
- * @author mikkel.laursen
+ * @author mlaursen
  * 
  */
 public class MyResultRow {
@@ -21,10 +21,46 @@ public class MyResultRow {
 		this.row.put(key, value);
 	}
 
+	/**
+	 * Returns a string from the row
+	 * @param key
+	 * @return
+	 */
 	public String get(String key) {
 		String col = this.row.get(key.toLowerCase());
 		return col == null ? this.row.get("test_"+key.toLowerCase()) : col;
-		//return this.row.get(key.toLowerCase());
+	}
+	
+	/**
+	 * Attempts to get an integer from the row
+	 * Returns 0 if it can not be parsed as an integer
+	 * or it is null
+	 * @param key
+	 * @return
+	 */
+	public int getInt(String key) {
+		try {
+			return Integer.parseInt(get(key));
+		}
+		catch(NumberFormatException | NullPointerException e) {
+			return 0;
+		}
+	}
+	
+	/**
+	 * Attempts to get a double from the row.
+	 * Returns 0.0 if it can not be parsed as a double or it is
+	 * null
+	 * @param key
+	 * @return
+	 */
+	public double getDouble(String key) {
+		try {
+			return Double.parseDouble(get(key));
+		}
+		catch(NullPointerException | NumberFormatException e) {
+			return 0;
+		}
 	}
 
 	public String toString() {
