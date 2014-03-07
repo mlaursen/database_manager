38227cfc6855efaf0d76aad24435b8cafb4a5e91
mlaursen <mlaursen03@gmail.com>
2014-03-07 15:23:32 -0500

    freshened up the sequence matcher

--- src/com/github/mlaursen/database/utils/SqlFormatUtil.java
+++ src/com/github/mlaursen/database/utils/SqlFormatUtil.java
@@ -6,7 +6,6 @@ package com.github.mlaursen.database.utils;
 import java.util.Arrays;
 import java.util.List;
 
-
 /**
  * @author mlaursen
  * 
@@ -14,53 +13,55 @@ import java.util.List;
 public class SqlFormatUtil {
 	
 	/**
-	 * Formats a sql create query to add TEST_ in front of all the tables and views and sequences.
+	 * Converts a create statement in sql for testing by prepending TEST_ before every key word given. The rule will be ignored if:
+	 * KEYWORD_X KEYWORD,
+	 * 
+	 * However, the rule will be followed if: SEQ_KEYWORD_X KEYWORD_VIEW KEYWORD_PKG KEYWORD
 	 * 
 	 * @param sqlToChange
-	 *            The string of sql to change
-	 * @param testingClasses
-	 *            The array of classes to append test_ to
-	 * @return Formatted sql string
+	 *            The sql to format for testing
+	 * @param keyWords
+	 *            A list of key words
+	 * @return The new "test" creation sql string
 	 */
-	public static String formatSqlForTesting(String sqlToChange, List<String> testingClasses) {
+	public static String formatSqlForTesting(String sqlToChange, List<String> keyWords) {
 		sqlToChange = sqlToChange.toUpperCase();
 		String[] lines = sqlToChange.split("\\r?\\n");
 		String pkg = "";
+		String words = "(";
+		for(int i = 0; i < keyWords.size(); i++) {
+			words += ClassUtil.combineWith(keyWords.get(i).split("\\_"), "\\_").toUpperCase();
+			words += i + 1 < keyWords.size() ? "|" : "";
+		}
+		words += ")";
 		for(int i = 0; i < lines.length; i++) {
 			String line = lines[i];
-			for(String s : testingClasses) {
-				if(s.length() > line.trim().length())
-					continue;
-				
-				s = s.toUpperCase();
-				String regex = ClassUtil.combineWith(s.split("\\_"), "\\_");
-				String myregex = "\\s(?=\\b"+regex+"((\\_VIEW|\\_PKG|[^_,])?)(?!,)\\b)";
-				String[] spaceSplits = line.split(myregex);
-				String s2 = "";
-				for(int j = 0; j < spaceSplits.length; j++) {
-					s2 += (j == 0 ? "" : " TEST_") + spaceSplits[j];
-				}
-				String[] seqs = s2.split("(?=\\s)?SEQ\\_(?=" + regex+")");
-				s2 = "";
-				for(int j = 0; j < seqs.length; j++) {
-					s2 += (j == 0 ? "" : "SEQ_TEST_") + seqs[j];
-				}
-				line = s2;
+			String[] splits = line.split("\\s(?=\\b" + words + "((\\_VIEW|\\_PKG|[^_,])?)(?!,)\\b)");
+			String s2 = "";
+			for(int j = 0; j < splits.length; j++) {
+				s2 += (j == 0 ? "" : " TEST_") + splits[j];
 			}
+			String[] seqs = s2.split("(?=\\s)?SEQ\\_(?=" + words + ")");
+			s2 = "";
+			for(int j = 0; j < seqs.length; j++) {
+				s2 += (j == 0 ? "" : "SEQ_TEST_") + seqs[j];
+			}
+			line = s2;
 			pkg += line + (i + 1 < lines.length ? "\n" : "");
 		}
 		return pkg;
 	}
+	
 	/**
 	 * Formats a sql create query to add TEST_ in front of all the tables and views and sequences.
 	 * 
 	 * @param sqlToChange
 	 *            The string of sql to change
-	 * @param testingClasses
-	 *            The array of classes to append test_ to
+	 * @param keyWords
+	 *            The array of key words to append test_ to
 	 * @return Formatted sql string
 	 */
-	public static String formatSqlForTesting(String sqlToChange, String[] testingClasses) {
-		return formatSqlForTesting(sqlToChange, Arrays.asList(testingClasses));
+	public static String formatSqlForTesting(String sqlToChange, String[] keyWords) {
+		return formatSqlForTesting(sqlToChange, Arrays.asList(keyWords));
 	}
 }
